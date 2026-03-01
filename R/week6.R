61cd3264c6bb9d0a6ad524c2ea4208aaa1288975
Tjknickel <knick071@umn.edu>
2026-03-01 12:59:24 -0600

    Finished data import

--- R/week6.R
+++ R/week6.R
@@ -1,6 +1,14 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(stringi)
+library(stringr)
 # library()
 # library()
-# library()
-# library()
+
+# Data Import 
+citations <- stri_read_lines(con = "../data/cites.txt")
+citations_txt <- citations[!stri_isempty(citations)]
+print(paste0("The number of blank lines eliminated was ", length(citations) - length(citations_txt)))
+print(paste0("The average number of characters/citation was ", mean(str_length(citations_txt))))
+
+# Data Cleaning 
